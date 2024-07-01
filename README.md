2e5059c470626445bee56ead875aee16d0c3b4be
digitalsleuth <corey@digitalsleuth.ca>
2024-07-01 02:49:43 +0000

    Major updates

--- README.md
+++ README.md
@@ -1,47 +1,83 @@
 # BinaryCookieReader
 
 Original author content found here: http://www.securitylearn.net/2012/10/27/cookies-binarycookies-reader/
+This repo was originally forked from: https://github.com/as0ler/BinaryCookieReader and updated for usage with Python 3
 
-This repo was forked from: https://github.com/as0ler/BinaryCookieReader
-
-Then updated for usage with Python 3
+Additional information can also be found at: https://github.com/libyal/dtformats/blob/master/documentation/Safari%20Cookies.asciidoc
+Table structure and contents sourced from: https://github.com/Lessica/CookiesTool
 
 ## Usage
 
-`
-binarycookiereader.py <filename>
-`
+```bash
+usage: binarycookiereader.py [-h] [-p] [-s SORT] [-V] [-v] <input_file>
 
-## Cookies.binarycookies Format
+BinaryCookieReader v2.0
 
-Cookies.binarycookies file is composed of several pages and each page can have one or more cookies.
+positional arguments:
+  <input_file>          Input file including path
 
-The complete file format is explained below:
+optional arguments:
+  -h, --help            show this help message and exit
+  -p, --pretty          Output into a clean table
+  -s SORT, --sort SORT  For pretty mode only: choose to sort by 'name','value','domain','create','expiry','flags'
+  -V, --verbose         Verbose mode: include name, value, and flags
+  -v, --version         show program's version number and exit
 
-### File Format:
-1. The file starts with a 4 byte magic string: cook. It is used to identify the file type.
-2. Next four bytes is an integer specifying the number of pages in the file.
-3. Following that, a 4 byte integer for each page, represents the page size.
-4. Next to that, the file contains the actual page content. Each page is of length corresponding to the page size. Page format is explained below.
-5. The file ends with an 8 byte value and it might be file checksum.`
+```
 
-### Page Format:
-1. Every page starts with a 4 byte page header: 0x00000100.
-2. Next four bytes is an integer specifying the number of cookies in the page.
-3. Following that, a 4 byte integer for each cookie, represents the cookie offset. Offset specifies the start of the cookie in bytes from the start of the page.
-4. Next to that, the page contains the actual cookie contents. Each cookie is of variable length. Cookie format is explained below.
-5. Page ends with a 4 byte value and it is always 0x00000000.
+## File Format
 
-### Cookie Format:
-1. First 4 bytes in the cookie is the size of the cookie.
-2. The next 4 bytes are unknown (may be related to cookies flags).
-3. The next four bytes are the cookie flags. This is an integer value (1=HTTPS, 4=HTTPOnly).
-4. The next 4 bytes are unknown.
-5. The next 4 bytes is an integer specifying the start of the url/domain field in bytes from the start of the cookie record.
-6. The next 4 bytes is an integer specifying the start of the name field in bytes from the start of the cookie record.
-7. The next 4 bytes is an integer specifying the start of the path field in bytes from the start of the cookie record.
-8. The next 4 bytes is an integer specifying the start of the value field in bytes from the start of the cookie record.
-9. The next 8 bytes represents the end of the cookie page and it is always 0x0000000000000000.
-10. The next 8 bytes are the cookie expiration date in Mac epoch format (Mac absolute time).
-11. The next 8 bytes are the cookie creation date in Mac epoch format (Mac absolute time).
-12. Next to that, the cookie contains the actual cookie domain, name, path & value. The order is not specific and they can appear in any order, thus the usage of the starting offsets.
+The *.binarycookies file is composed of several pages and each page can have one or more cookies.
+
+### File
+
+| Field           | Endianness | Type                 | Size        | Description                             | Sample        |
+|-----------------|------------|----------------------|-------------|-----------------------------------------|---------------|
+| Magic           | BE         | UTF-8                | 4           | ASCII characters "cook"                 |  63 6F 6F 6B  |
+| Number of pages | BE         | Unsigned Int         | 4           |                                         |  00 00 01 96  |
+| Page 1 size     | BE         | Unsigned Int         | 4           | Size of Page 1                          |  00 00 00 6F  |
+
+(Repeat for # pages * 4 bytes to get to first page)
+
+### Page
+
+| Field                        | Endianness | Type         | Size          | Description          | Sample        |
+|------------------------------|------------|--------------|---------------|----------------------|---------------|
+| Header                       | BE         |              | 4             | 0x00000100           |  00 00 01 00  |
+| Number of cookies in page    | LE         | Unsigned Int | 4             |                      |  01 00 00 00  |
+| Cookie N offset incl. Header | LE         | Unsigned Int | 4             | Repeat for N cookies |  10 00 00 00  |
+| Header end                   | LE         |              | 4             | 0x00000000           |  00 00 00 00  |
+
+(Cookie Content Starts)
+
+### Cookie Content
+
+| Field              | Endianness | Type         | Size | Description                                                                  | Sample                    |
+|--------------------|------------|--------------|------|------------------------------------------------------------------------------|---------------------------|
+| Size               | LE         | Unsigned Int | 4    | Size in bytes                                                                |  5F 00 00 00              |
+| Version            | LE         | Unsigned Int | 4    | 0 or 1                                                                       |  00 00 00 00              |
+| Flags              | LE         | Bit field    | 4    | isSecure = 1, isHTTPOnly = 1 << 2, unknown1 = 1 << 3, unknown2 = 1 << 4      |  01 00 00 00              |
+| Has port           | LE         | Unsigned Int | 4    | 0 or 1                                                                       |  00 00 00 00              |
+| URL Offset         | LE         | Unsigned Int | 4    | Offset from the start of the cookie                                          |  38 00 00 00              |
+| Name Offset        | LE         | Unsigned Int | 4    | Offset from the start of the cookie                                          |  4C 00 00 00              |
+| Path Offset        | LE         | Unsigned Int | 4    | Offset from the start of the cookie                                          |  5B 00 00 00              |
+| Value Offset       | LE         | Unsigned Int | 4    | Offset from the start of the cookie                                          |  5D 00 00 00              |
+| Comment Offset     | LE         | Unsigned Int | 4    | Offset from the start of the cookie, 0x00000000 if not present               |  00 00 00 00              |
+| Comment URL Offset | LE         | Unsigned Int | 4    | Offset from the start of the cookie, 0x00000000 if not present               |  00 00 00 00              |
+| Expiration         | LE         | Double       | 8    | Number of seconds since 00:00:00 UTC on 1 January 2001                       |  00 00 00 AD 5A 07 C8 41  |
+| Creation           | LE         | Double       | 8    | Number of seconds since 00:00:00 UTC on 1 January 2001                       |  00 00 00 5A B2 42 BD 41  |
+| Port               | LE         | Unsigned Int | 2    | Only present if the "Has port" field is 1                                    |                           |
+| Comment            | LE         | String       |      | Null-terminated, optional                                                    |                           |
+| Comment URL        | LE         | String       |      | Null-terminated, optional                                                    |                           |
+| URL                | LE         | String       |      | Null-terminated                                                              |  www.knowwhere.com        |
+| Name               | LE         | String       |      | Null-terminated                                                              |  _guages_unique.          |
+| Path               | LE         | String       |      | Null-terminated                                                              |  /                        |
+| Value              | LE         | String       |      | Null-terminated                                                              |  .                        |
+
+### End of File
+
+| Field           | Endianness | Type                 | Size        | Description                             | Sample                    |
+|-----------------|------------|----------------------|-------------|-----------------------------------------|---------------------------|
+| Checksum        | BE         | Unsigned Int         | 4           | Sum every 4th byte for each page        |  00 00 2F F2              |
+| Footer          | BE         |                      | 8           | 0x07172005000000(45\|4b)                |  07 17 20 05 00 00 00 45  |
+| Metadata        |            | Binary Property List | TO EOF      | Contains NSHTTPCookieAcceptPolicy value |  62 70 6C 69 73 74 30 30  |
---
+++ binarycookiereader/binarycookiereader.py
@@ -0,0 +1,289 @@
+#!/usr/bin/env python3
+
+import sys
+from struct import unpack
+from io import BytesIO
+from datetime import datetime as dt
+from argparse import ArgumentParser, RawTextHelpFormatter
+from urllib.parse import unquote
+from pathlib import Path
+from prettytable import *
+
+__maintainer__ = "Corey Forman"
+__original_author__ = "Satishb3"
+__source__ = "https://github.com/digitalsleuth/binarycookiereader"
+__reference__ = "Script from Satishb3 (http://www.securitylearn.net)"
+__version__ = "2.0"
+__date__ = "30 June 2024"
+__description__ = "Python 3 Parser for the Cookies.binarycookie plist file"
+__about__ = (
+    "Safari and iOS applications store their persistent cookies in a binary plist file named  \n"
+    + "Cookies.binarycookies. This script will dump all of the cookies from this file.      \n\n"
+    + "This is a Python 3 implementation of the original script found here:                 \n\n"
+    + "https://github.com/as0ler/BinaryCookieReader and here:                                 \n"
+    + "http://www.securitylearn.net/2012/10/27/cookies-binarycookies-reader/                \n\n"
+    + "More information about the format can be found here:                                   \n"
+    + "https://github.com/libyal/dtformats/blob/master/documentation/Safari%20Cookies.asciidoc\n"
+    + "and https://github.com/digitalsleuth/BinaryCookieReader/blob/master/FORMAT.md       \n \n"
+)
+
+
+def process(binary_file, file_size):
+    check_header(binary_file)
+    parse_pages(binary_file, file_size)
+    binary_file.close()
+
+
+def check_header(binary_file):
+    bc_header = b"cook"
+    file_header = binary_file.read(4)
+    if file_header != bc_header:
+        print(
+            f"Invalid binarycookie header: {file_header} - should start with '0x636f6f6b (cook)'"
+        )
+        raise SystemExit(1)
+    else:
+        pass
+
+
+def parse_pages(binary_file, file_size):
+    tbl_output = []
+    csv_output = []
+    print(f"Filename: {Path(args.input_file).resolve()}\n")
+    if args.verbose:
+        csv_output.append(["Name,Value,Domain and Path,Create Date,Expiry Date,Flags"])
+        tbl_output.append(
+            ["Name", "Value", "Domain and Path", "Create Date", "Expiry Date", "Flags"]
+        )
+    else:
+        csv_output.append(["Domain and Path,Create Date,Expiry Date"])
+        tbl_output.append(["Domain and Path", "Create Date", "Expiry Date"])
+    epoch_diff = 978307200  # 978307200 is the diff between unix epoch and 1/Jan/2001
+    bplist_offset = 8
+    num_pages = unpack(">i", binary_file.read(4))[
+        0
+    ]  # Number of pages in the binary file: 4 bytes
+    bplist_offset += num_pages * 4
+    page_sizes = []
+    for np in range(num_pages):
+        page_size = unpack(">i", binary_file.read(4))[0]
+        page_sizes.append(page_size)  # Each page size is 4 bytes * number of pages
+        bplist_offset += page_size
+    pages = []
+    for ps in page_sizes:
+        pages.append(
+            binary_file.read(ps)
+        )  # Grab individual pages and each page will contain at least one cookie
+    for page in pages:
+        page = BytesIO(page)
+        page.read(4)  # Page header: 4 bytes: Always 0x00000100
+        num_cookies = unpack("<i", page.read(4))[
+            0
+        ]  # Number of cookies in each page, first 4 bytes after the page header in every page.
+        cookie_offsets = []
+        for nc in range(num_cookies):
+            cookie_offsets.append(
+                unpack("<i", page.read(4))[0]
+            )  # Every page contains at least one cookie. Fetch cookie starting point from page starting byte
+        page.read(4)  # End of page header: Always 00000000
+        cookie = ""
+        for offset in cookie_offsets:
+            page.seek(offset)  # Move the page pointer to the cookie starting point
+            cookiesize = unpack("<i", page.read(4))[0]  # Fetch cookie size
+            cookie = BytesIO(page.read(cookiesize))
+            cookie.read(4)
+
+            flags = unpack("<i", cookie.read(4))[
+                0
+            ]  # Cookie flags:  1=HTTPS, 4=HTTP, 5=HTTPS and HTTP
+            cookie_flags = ""
+            if flags == 0:
+                cookie_flags = "None"
+            elif flags == 1:
+                cookie_flags = "HTTPS"
+            elif flags == 4:
+                cookie_flags = "HTTP"
+            elif flags == 5:
+                cookie_flags = "HTTPS and HTTP"
+            else:
+                cookie_flags = "Unknown Flag"
+
+            has_port = cookie.read(4)  # If the cookie has a port assigned
+            urloffset = unpack("<i", cookie.read(4))[
+                0
+            ]  # Cookie domain offset from cookie starting point
+            nameoffset = unpack("<i", cookie.read(4))[
+                0
+            ]  # Cookie name offset from cookie starting point
+            pathoffset = unpack("<i", cookie.read(4))[
+                0
+            ]  # Cookie path offset from cookie starting point
+            valueoffset = unpack("<i", cookie.read(4))[
+                0
+            ]  # Cookie value offset from cookie starting point
+            endofcookie = cookie.read(8)  # End of cookie
+            expiry_date_epoch = (
+                unpack("<d", cookie.read(8))[0] + epoch_diff
+            )  # Expiry date is in Mac epoch format: Starts from 1/Jan/2001
+            create_date_epoch = (
+                unpack("<d", cookie.read(8))[0] + epoch_diff
+            )  # Cookie creation time
+
+            cookie.seek(urloffset - 4)  # Fetch domain value from url offset
+            url = ""
+            u = cookie.read(1)
+            while unpack("<b", u)[0] != 0:
+                url = url + bytes.decode(u)
+                u = cookie.read(1)
+            cookie.seek(nameoffset - 4)  # Fetch cookie name from name offset
+            name = ""
+            n = cookie.read(1)
+            while unpack("<b", n)[0] != 0:
+                name = name + bytes.decode(n)
+                if "%" in name:
+                    name = unquote(name)
+                n = cookie.read(1)
+            cookie.seek(pathoffset - 4)  # Fetch cookie path from path offset
+            path = ""
+            pa = cookie.read(1)
+            while unpack("<b", pa)[0] != 0:
+                path = path + bytes.decode(pa)
+                pa = cookie.read(1)
+            cookie.seek(valueoffset - 4)  # Fetch cookie value from value offset
+            value = ""
+            va = cookie.read(1)
+            while unpack("<b", va)[0] != 0:
+                value = value + bytes.decode(va)
+                if "%" in value:
+                    value = unquote(value)
+                va = cookie.read(1)
+            if args.verbose:
+                csv_output.append(
+                    [
+                        f"{name},{value},{url}{path},{int(create_date_epoch)},{int(expiry_date_epoch)},{cookie_flags}"
+                    ]
+                )
+                tbl_output.append(
+                    [
+                        f"{name}",
+                        f"{value}",
+                        f"{url}{path}",
+                        f"{int(create_date_epoch)}",
+                        f"{int(expiry_date_epoch)}",
+                        f"{cookie_flags}",
+                    ]
+                )
+            else:
+                csv_output.append(
+                    [f"{url}{path},{int(create_date_epoch)},{int(expiry_date_epoch)}"]
+                )
+                tbl_output.append(
+                    [
+                        f"{url}{path}",
+                        f"{int(create_date_epoch)}",
+                        f"{int(expiry_date_epoch)}",
+                    ]
+                )
+    bplist_offset += 12  # 4 bytes for the checksum, 8 bytes for the footer signature
+    if args.bplist and file_size > bplist_offset:
+        binary_file.seek(bplist_offset)
+        if binary_file.read(8) == b"bplist00":
+            binary_file.seek(bplist_offset)
+            bplist = binary_file.read(file_size - bplist_offset)
+            bplist_file = open(f"{args.input_file}.bplist", "wb")
+            bplist_file.write(bplist)
+            bplist_file.close()
+    if args.bplist and file_size <= bplist_offset:
+        print("--bplist was selected, but there is no bplist at the end of the file")
+    if args.table:
+        tbl = PrettyTable(tbl_output[0])
+        tbl.set_style(SINGLE_BORDER)
+        tbl.align = "l"
+        tbl.add_rows(tbl_output[1:])
+        sort_by = "Domain and Path"
+        if args.sort:
+            sort_col = (args.sort).lower()
+            if "name" in sort_col:
+                sort_by = "Name"
+            elif "value" in sort_col:
+                sort_by = "Value"
+            elif "domain" in sort_col:
+                sort_by = "Domain and Path"
+            elif "create" in sort_col:
+                sort_by = "Create Date"
+            elif "expiry" in sort_col:
+                sort_by = "Expiry Date"
+            elif "flags" in sort_col:
+                sort_by = "Flags"
+        if not sort_by in tbl.field_names:
+            print(
+                f"{sort_by} is not available in this view, defaulting to sort by 'Domain and Path'"
+            )
+            sort_by = "Domain and Path"
+        tbl.sortby = sort_by
+        tbl.custom_format["Create Date"] = lambda f, v: dt.utcfromtimestamp(
+            int(v)
+        ).strftime("%Y-%m-%d %H:%M:%S")
+        tbl.custom_format["Expiry Date"] = lambda f, v: dt.utcfromtimestamp(
+            int(v)
+        ).strftime("%Y-%m-%d %H:%M:%S")
+        print(tbl)
+    else:
+        for each_entry in csv_output:
+            print(",".join(each_entry))
+
+
+def main():
+    arg_parse = ArgumentParser(
+        description="BinaryCookieReader v" + __version__,
+        epilog=str(__about__),
+        formatter_class=RawTextHelpFormatter,
+    )
+    arg_parse.add_argument(
+        "input_file", metavar="<input_file>", help="Input file including path"
+    )
+    arg_parse.add_argument(
+        "-b",
+        "--bplist",
+        dest="bplist",
+        action="store_true",
+        help="If a bplist exists at the end of the .binarycookies file, export it as <filename>.bplist",
+    )
+    arg_parse.add_argument(
+        "-t",
+        "--table",
+        dest="table",
+        action="store_true",
+        help="Output into a clean table",
+    )
+    arg_parse.add_argument(
+        "-s",
+        "--sort",
+        dest="sort",
+        help="For table output only: choose to sort by 'name','value','domain','create','expiry','flags'",
+    )
+    arg_parse.add_argument(
+        "-V",
+        "--verbose",
+        dest="verbose",
+        action="store_true",
+        help="Verbose mode: include name, value, and flags",
+    )
+    arg_parse.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version="%(prog)s" + " v" + str(__version__),
+    )
+    args = arg_parse.parse_args()
+
+    try:
+        binary_file = open(args.input_file, "rb")
+        file_size = Path(args.input_file).stat().st_size
+        process(binary_file, file_size)
+    except IOError as e:
+        print(f"Unable to read '{args.input_file}': {e}")
+        raise SystemExit(1)
+
+if __name__ == "__main__":
+    main()
